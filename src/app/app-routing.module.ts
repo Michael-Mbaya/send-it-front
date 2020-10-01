7a4a118647eda826982ecf4ae29182ba27f86238
Michael-Mbaya <gikundamike@gmail.com>
2020-10-01 10:33:50 +0300

    commit one

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sign-up', component:SignupComponent},
+  {path:'sign-in', component:SigninComponent},
+  {path:'about', component:AboutUsComponent},
+  {path: '**', component:NotFoundComponent},
+  // redirect
+  {path: '',redirectTo:"/sign-in",pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    NotFoundComponent,
+    NavComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
